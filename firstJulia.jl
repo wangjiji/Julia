702a7e95858c471892b9759545b16f9e24f559ba
wangjiji <948064376@qq.com>
2017-03-12 08:39:55 +0800

    Julia

---
+++ firstJulia.jl
@@ -0,0 +1,9 @@
+# ; 进入shell 模式  cd到目前文件夹
+# include("firstJulia.jl")
+
+println("Hello,World!")
+x = 1:0.1:pi;
+y=sin(x);
+# println(y)
+println("over")
+
---
+++ hello.jl
@@ -0,0 +1,5 @@
+#Julia
+julia 的启动方式：
+1.进入到目录下，Julia>>>include("hello.jl")
+2.进入到目录下，在终端非交互模式下 julia hello.jl
+3.sublime text
