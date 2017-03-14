099f7a454cd241551b507cbe86ddaa48d58d393a
wangjiji <948064376@qq.com>
2017-03-14 14:45:05 +0800

    .jl add

---
+++ drawfig.jl
@@ -0,0 +1,7 @@
+import Plots
+x=10
+function plo(arg::Int16)
+    Plots.plot(cos)
+    arg = 11
+    
+end
--- firstJulia.jl
+++ firstJulia.jl
@@ -1,9 +1,12 @@
 # ; 进入shell 模式  cd到目前文件夹
 # include("firstJulia.jl")
 
+#=using Plots
+plot(cos)
+=#
 println("Hello,World!")
-x = 1:0.1:pi;
-y=sin(x);
+x = 1:0.1:pi
+y=sin(x)
 # println(y)
 println("over")
 
