6c0653b6c1b07cda44ec670e84cbee32689b3342
wangjiji <948064376@qq.com>
2017-03-28 19:24:57 +0800

    change ieeekillcol.jl

--- ieeekillcol.jl
+++ ieeekillcol.jl
@@ -9,9 +9,10 @@ function oneieeecol!(ven::Float64,ret::AbstractVector{Float64},i::Int64;d::Float
     cc = 299792458
     ven = 2pi*ven
     
-    wp = 2pi*2e9                ##plasma frequency
+    wp = 2pi*1e9                ##plasma frequency
 
-    wpx = [0.01:0.01:0.5;0.55:0.05:2.5;2.8:0.3:10;10:0.5:50;52:2:100]    ##2e7-2e11Hz
+    wpx = [0.01:0.01:0.5;0.51:0.02:2.5;2.52:0.02:10;10.02:0.05:50;50.5:0.5:200]    ##1e7-2e11Hz
+    #wpx=linspace(1,100,5000)
     wplen = length(wpx)
 
     er = ones(wplen)
@@ -37,13 +38,13 @@ function oneieeecol!(ven::Float64,ret::AbstractVector{Float64},i::Int64;d::Float
 end
 
 ##different collision frequency
-function coldiff()
+function coldiff(;fre::Float64=2.0e9,d::Float64=0.05)
 
     ven = 2e9
     # venx = [0.01:0.01:0.5;0.55:0.05:2.5;2.8:0.3:10;10:0.5:20;
     # 21:1:50;52:2:100;105:10:500;520:20:1000;1100:200:10000]*ven           ##2e7-2e13Hz
 
-    venx0 = linspace(10,11,5000)
+    venx0 = linspace(7,13,5000)
     venx = 10.^venx0
 
     velen = length(venx)
@@ -51,12 +52,12 @@ function coldiff()
     ret = ones(velen)
     for xx =1:velen
 
-        ωₚ[xx]=oneieeecol!(venx[xx],ret,xx)
+        ωₚ[xx]=oneieeecol!(venx[xx],ret,xx;fre=fre,d=d)
 
     end
 
     # print(ωₚ[1:10:end],"\n",ret[1:10:end])
-    PyPlot.plot(linspace(1e10,1e11,5000),ret)
+    PyPlot.plot(#=venx0,ret)=#venx0,ωₚ)
 
 
     
